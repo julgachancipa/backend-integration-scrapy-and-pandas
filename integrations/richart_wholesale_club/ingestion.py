40d6eac9820e9bb5d945f3279260498c038d152c
julgachancipa <julianaga01@gmail.com>
2020-10-04 22:26:52 -0500

    [UPDATE] - load session, capitalize strings

--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -1,16 +1,27 @@
 import os
-import numpy as np
 import pandas as pd
-
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import engine
-from models import BranchProduct, Product, Base
+from models import BranchProduct, Product
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 ASSETS_DIR = os.path.join(PROJECT_DIR, "assets")
 PRODUCTS_PATH = os.path.join(ASSETS_DIR, "PRODUCTS.csv")
 PRICES_STOCK_PATH = os.path.join(ASSETS_DIR, "PRICES-STOCK.csv")
 BRANCHES = ['MM', 'RHSM']
+
+
+def load_session():
+    """
+    It loads the SQLAlquemy session
+    :return: session
+    """
+    dbPath = PROJECT_DIR + '/db.sqlite'
+    engine = create_engine('sqlite:///%s' % dbPath, echo=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
 
 
 def process_csv_files():
@@ -31,6 +42,10 @@ def select_products_df(products_df):
     :param products_df: products data frame
     :return: filtered products data frame
     """
+    products_df['CATEGORY'] = products_df['CATEGORY'].str.capitalize()
+    products_df['SUB_CATEGORY'] = products_df['SUB_CATEGORY'].str.capitalize()
+    products_df['SUB_SUB_CATEGORY'] = products_df['SUB_SUB_CATEGORY'].str.capitalize()
+
     full_category = ['CATEGORY', 'SUB_CATEGORY', 'SUB_SUB_CATEGORY']
     products_df['FULL_CATEGORY'] = products_df[full_category]\
         .apply(lambda row: '|'.join(row.values.astype(str)), axis=1)
@@ -61,12 +76,9 @@ def products_to_db(products):
     It saves the products in the database
     :param products: dictionary with the desired information
     """
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base.metadata.create_all(engine)
-
+    session = load_session()
     for key, item in products.items():
-        print('Processing:', key, item['NAME'])
+        print('\n>>> Processing:', key, item['NAME'])
         product = (
             session.query(Product)
             .filter_by(store="Richart's", sku=item["SKU"])
@@ -77,12 +89,13 @@ def products_to_db(products):
             product = Product(store="Richart's", sku=item["SKU"])
 
         product.barcodes = item["BARCODES"]
-        product.brand = item["BRAND"]
-        product.name = item["NAME"]
-        product.description = item["DESCRIPTION"]
+        product.brand = item["BRAND"].capitalize()
+        product.name = item["NAME"].capitalize()
+        description = remove_html_tags(item["DESCRIPTION"])
+        product.description = description.capitalize()
         product.image_url = item["IMAGE_URL"]
         product.category = item["FULL_CATEGORY"]
-        product.package = item["DESCRIPTION"].strip(item["NAME"])
+        product.package = product.description.replace(product.name, '')
 
         session.add(product)
         session.commit()
@@ -106,6 +119,16 @@ def products_to_db(products):
     session.close()
 
 
+def remove_html_tags(text):
+    """
+    Remove html tags from a string
+    :return Cleaned string
+    """
+    import re
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+
 if __name__ == "__main__":
     products_df, prices_stock_df = process_csv_files()
 
@@ -113,7 +136,7 @@ if __name__ == "__main__":
     prices_stock_df = select_prices_stock_df(prices_stock_df)
 
     full_products_df = pd.merge(products_df, prices_stock_df, on='SKU')
-    full_products_df.drop_duplicates(keep=False,inplace=True)
+    full_products_df.drop_duplicates(keep=False, inplace=True)
 
     products = full_products_df.to_dict('index')
     products_to_db(products)
